17dafd350b768532bc6e028150d634e55faf641d
szrpf <27185709@qq.com>
2023-06-03 12:32:58 +0800

    v1.0

--- assets/Script/SceneGame.ts
+++ assets/Script/SceneGame.ts
@@ -7,7 +7,7 @@ export default class SceneGame extends cc.Component {
     richText:RichText = null;
     start () {
         this.richText = cc.find('富文本',this.node).getComponent(RichText);
-        // this.richText.string = '\\p爆炸\\p你好';
+        // this.richText.string = '这是一个\\pBullet\\p富文本\\p爆炸\\p组件';
     }
 
 }
